e1784ba609202e41e4fb3ad10c3b12328000b70f
Vladislav Semenov <arctic.ratatat@gmail.com>
2022-01-24 18:01:50 +0300

    rescheduled test failed

--- src/test/java/ru/netology/web/DateMeeting.java
+++ src/test/java/ru/netology/web/DateMeeting.java
@@ -4,8 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateMeeting {
+
     static String dateInput(int days) {
         String inputDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return inputDate;
+    }
+
+    static String dateInputNext(int daysNext) {
+        String inputDateNext = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return inputDateNext;
     }
 }
--- src/test/java/ru/netology/web/TestOrderForm.java
+++ src/test/java/ru/netology/web/TestOrderForm.java
@@ -1,7 +1,8 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -14,36 +15,44 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestOrderForm {
 
     public int days;
+    public int daysNext;
+
+    @BeforeEach
+    void shouldPrepare(){
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
+
+    @AfterEach
+    void shouldClose() {
+        closeWindow();
+    }
 
     @Test
     public void shouldAccepted() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Краснодар");
         $("[data-test-id=date] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] [placeholder='Дата встречи']").setValue(DateMeeting.dateInput(days));
-        $("[data-test-id=name] [name='name']").setValue("Валерий Фокин-Хайкин");
+        $("[data-test-id=name] [name='name']").setValue("Валерий Градский");
         $("[data-test-id=phone] [name='phone']").setValue("+79296969292");
         $("[class=checkbox__box]").click();
         $(withText("Запланировать")).click();
-        $(".notification__content").shouldBe(visible, Duration.ofSeconds(5))
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(3))
                 .shouldHave(exactText("Встреча успешно запланирована на " + DateMeeting.dateInput(days)));
     }
 
     @Test
-    public void shouldReschedule() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
+    public void shouldRescheduled() {
         $("[data-test-id=city] input").setValue("Краснодар");
         $("[data-test-id=date] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [placeholder='Дата встречи']").setValue(DateMeeting.dateInput(days));
-        $("[data-test-id=name] [name='name']").setValue("Валерий Фокин-Хайкин");
+        $("[data-test-id=date] [placeholder='Дата встречи']").setValue(DateMeeting.dateInputNext(daysNext));
+        $("[data-test-id=name] [name='name']").setValue("Валерий Градский");
         $("[data-test-id=phone] [name='phone']").setValue("+79296969292");
         $("[class=checkbox__box]").click();
         $(withText("Запланировать")).click();
-        $(withText("Перепланировать?")).shouldBe(Condition.visible, Duration.ofSeconds(5));
-        $$("[class=button__text]").find(exactText("Перепланировать")).click();
-        $(".notification__content").shouldBe(visible, Duration.ofSeconds(5))
-                .shouldHave(exactText("Встреча успешно запланирована на " + DateMeeting.dateInput(days)));
+        $(withText("Перепланировать?")).shouldBe(visible, Duration.ofSeconds(3));
+        $$("button").find(exactText("Перепланировать")).click();
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(3))
+                .shouldHave(exactText("Встреча успешно запланирована на " + DateMeeting.dateInputNext(daysNext)));
     }
 }
